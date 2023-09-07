95771e2423fb9d3590b7c784f9c798fa2e814c2d
Rainzler Exagone <Yasser458891@gmail.com>
2023-09-08 01:45:32 +0200

    Complete

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import './style.css';
+import gsap from "gsap" 
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
+
 
 
 //Scene
@@ -37,13 +39,16 @@ scene.add(light)
 const canvas = document.querySelector(".webgl")
 const renderer = new THREE.WebGLRenderer({ canvas })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(2)
 renderer.render(scene, camera)
 
 //Controls (spin using cursor)
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping= true
-controls.enablePan= false
-controls.enableZoom= false
+controls.enablePan = false
+controls.enableZoom = false
+controls.autoRotate = true
+controls.autoRotateSpeed = 5
 
 //Resize
 window.addEventListener('resize',()=>{
@@ -65,3 +70,34 @@ const loop = () => {
     window.requestAnimationFrame(loop)
 }
 loop()
+
+//Tineline magic
+const tl = gsap.timeline({defaults: {duration: 1}})
+tl.fromTo(mesh.scale,{z:0, x:0, y:0}, {z:1, x:1, y:1})
+tl.fromTo('nav' , {y:'-100%'},{y:"0%"})
+tl.fromTo(".title",{opacity: 0},{opacity: 1 })
+
+//Mouse Animation colorrrr
+let mouseDown = false
+let rgb = [12, 23, 55]
+window,addEventListener("mousedown",()=>(mouseDown = true))
+window,addEventListener("mouseup",()=>(mouseDown = false))
+
+window.addEventListener('mousemove' , (e) => {
+    if(mouseDown){
+       
+        rgb = [
+            Math.round((e.pageX / sizes.width) * 255),
+            Math.round((e.pageY / sizes.height) * 255),
+                150,]
+                console.log(rgb)
+    //let's animate
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+    gsap.to(mesh.material.color, {
+        r:newColor.r,
+        g:newColor.g,
+        b:newColor.b,
+    })
+        
+    }
+})
